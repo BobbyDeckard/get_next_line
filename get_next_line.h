4d7529ec27dd34fe526cead45f5ba5547c012445
Ivo Meulemans <imeulema@c2r8s6.42lausanne.ch>
2024-10-29 14:22:51 +0100

    Added prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: imeulema <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:18:10 by imeulema          #+#    #+#             */
-/*   Updated: 2024/10/29 14:19:02 by imeulema         ###   ########.fr       */
+/*   Updated: 2024/10/29 14:22:39 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+char	*get_next_line(int fd);
 
 #endif
